55f3f640da9cd4d06f2c2ebe5d4914a741d63b1f
Cody Stephenson <cdystphnsn@gmail.com>
2023-10-12 20:39:23 -0700

    Include markdown, and provide example treesitter manual install
    Having to google sucks

--- init.lua
+++ init.lua
@@ -402,8 +402,8 @@ vim.keymap.set('n', '<leader>sd', '<cmd>Telescope live_grep cwd=$HOME/Documents/
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim',
-    'sql' },
+  -- alternative, manual install ==> :TSInstall markdown
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'sql', 'markdown', 'markdown_inline' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
