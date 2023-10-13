81c5f4d4b80c61b7e707057c4f352b721d05a88c
Cody Stephenson <cdystphnsn@gmail.com>
2023-10-12 20:39:55 -0700

    Changes to switch to indent-blankline v3

--- init.lua
+++ init.lua
@@ -196,12 +196,10 @@ require('lazy').setup({
   {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
+    main = "ibl",
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = true,
-    },
+    opts = {},
   },
 
   -- "gc" to comment visual regions/lines
