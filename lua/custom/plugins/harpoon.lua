b223766189f4faa026ab7f929d07b85e75950d7d
zenxr <cdystphnsn@gmail.com>
2025-05-22 14:04:37 -0700

    Dvorak harpoon keymaps

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -9,9 +9,9 @@ return {
     local ui = require("harpoon.ui")
     local mark = require("harpoon.mark")
 
-    vim.keymap.set('n', '<leader>jl', function() ui.toggle_quick_menu() end, { desc = '[L]ist Harpoon'})
-    vim.keymap.set('n', '<leader>jj', function() ui.nav_next() end, { desc = 'Next Harpoon'})
-    vim.keymap.set('n', '<leader>jf', function() ui.nav_prev() end, { desc = 'Prev Harpoon'})
-    vim.keymap.set('n', '<leader>jk', function() mark.add_file() end, { desc = 'Add Harpoon'})
+    vim.keymap.set('n', '<leader>hl', function() ui.toggle_quick_menu() end, { desc = '[L]ist Harpoon'})
+    vim.keymap.set('n', '<leader>hh', function() ui.nav_next() end, { desc = 'Next Harpoon'})
+    vim.keymap.set('n', '<leader>hu', function() ui.nav_prev() end, { desc = 'Prev Harpoon'})
+    vim.keymap.set('n', '<leader>ht', function() mark.add_file() end, { desc = 'Add Harpoon'})
   end,
 }
