82d080f759f2eeff9802db893558eaad52648bb7
zenxr <cdystphnsn@gmail.com>
2025-06-17 17:50:26 -0700

    Trying out disabling autopairs
    Autopairs often gets annoying, how much work will typing brackets/parens
    really be?

--- lua/custom/plugins/autopairs.lua
+++ lua/custom/plugins/autopairs.lua
@@ -1,9 +1,8 @@
--- File: lua/custom/plugins/autopairs.lua
-
 return {
   "windwp/nvim-autopairs",
   -- Optional dependency
   dependencies = { 'hrsh7th/nvim-cmp' },
+  cond = false,
   config = function()
     require("nvim-autopairs").setup {}
     -- If you want to automatically add `(` after selecting a function or method
